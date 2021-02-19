3a4a7faecc7ab0b95513fa5dd714309fabc77601
Vasilis The Pikachu <vasilis@thepikachu.ga>
2021-02-19 18:00:21 +0100

    Fixed a bug when you reset an image after resizing it also removed some useless code again

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ gunshot = pygame.mixer.Sound('gunshot.mp3')
 idiot = pygame.image.load(file)
 screen = pygame.display.set_mode(idiot.get_rect().size, pygame.RESIZABLE)
 screen.fill((255, 255, 255))
-idiot.convert()
 screen.blit(idiot, (10, 10))
 pygame.display.flip()
 
@@ -37,17 +36,14 @@ while running:
         # If R is pressed reset the screen
         if pygame.key.get_pressed()[pygame.K_r] == 1:
             screen.fill((255, 255, 255))
-            idiot = pygame.image.load(file)
-            idiot.convert()
+            w, h = pygame.display.get_surface().get_size()
+            idiot = pygame.transform.scale(idiot, (w, h))
             screen.blit(idiot, (10, 10))
             pygame.display.flip()
         # If the window is resized resize the image and window
         if event.type == pygame.VIDEORESIZE:
-            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
             screen.fill((255, 255, 255))
-            idiot = pygame.image.load(file)
             idiot = pygame.transform.scale(idiot, (event.w, event.h))
-            idiot.convert()
             screen.blit(idiot, (10, 10))
             pygame.display.flip()
 
